8184e67e4cd3179ee2f80d7d5e9a1db72b9c6206
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-04-04 03:25:50 +0300

    display text in previewer when typing in editor

---
+++ src/components/Editor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Editor(props) {
+    const handleInputChange = (e) => {
+        props.setInput(e.target.value)
+    }
+    return (
+        <textarea id='editor' value={props.input} onChange={handleInputChange}></textarea>
+    )
+}
+
+export default Editor
---
+++ src/components/Previewer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Previewer(props) {
+  return (
+    <div id='preview'>{props.input}</div>
+  )
+}
+
+export default Previewer
